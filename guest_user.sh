be395d05b36e5b948fa5d9024200f919871754b5
Terosami <gregor@holzfeind.ch>
2019-03-27 11:48:51 +0100

    Update guest_user.sh
    Auslagerung Analayse Funktionen

--- guest_user.sh
+++ guest_user.sh
@@ -1,3 +1,4 @@
+
 #/bin/bash
 
 #    This program is free software: you can redistribute it and/or modify
@@ -32,25 +33,60 @@
 
 ##########################################################################################
 
-#   Anpassbare Variablen
+#    Anpassbare Variablen
 
 log_location="/var/log/192.168.0.8/syslog.log"    # Speicherpfad der Syslogdatei
 ssid="Test_Gast"    # zu Anlalysierende SSID
 databas="csv"    # Speichervariante der Enddaten. Mögliche Werte: csv, sql
-mac_only="false"    # Nur MAC-Adressen speichern, falls der Wert "false" ist werden auch die Benutzernamen erfasst.
+mac_only="false"    # Nur MAC-Adressen speichern = true. MAC-Adresse und Benutzername speichern = false.
 
 #########################################################################################
 
-#   Funktionen
+#    Funktionen
 
 function func_analyse(){
         cat $log_location | grep "$ssid" | grep "User" > log_redu.tmp
         while read input
         do
-                echo ""
-        done < log_redu.tmp
+                # Type des Log-Eintrag ermitteln
+                func_log_typ
+                # MAC-Adresse und Benutzername ermitteln
+                #func_mac_user
+        done < test.log #log_redu.tmp
 }
 
+function func_mac_user(){
+        if [ "$mac_only" = "true" ]
+        then
+                mac=$(echo $input | cut -d"[" -f2 | cut -d"]" -f1)
+        elif [ "$mac_only" = "false" ]
+        then
+                mac=$(echo $input | cut -d"[" -f2 | cut -d"]" -f1 | cut -d"@" -f2)
+                username=$(echo $input | cut -d"[" -f2 | cut -d"]" -f1 | cut -d"@" -f1)
+        else
+                echo "Falscher Wert bei der Variabel " '"$mac_only".'
+                echo "Der Aktuelle Wert ist: " $mac_only
+                echo -e "\nDie einzig zugelassen Werte sind: true oder false"
+                echo -e "\nDen Wert im Script anpassen und erneut durchführen"
+                echo -e "\n\nDas Script wird nun beendet und ein entsprechender Log-Eintrag wird angelegt"
+                logger guest_user.sh: error mac_only wronge value. ssid: $ssid, mac_only: $mac_only
+                exit
+        fi
+}
+
+function func_log_typ(){
+        joins_e=$(echo $input | grep "joins" | cut -d" " -f1)
+        joins_d=$(echo $input | grep "tritt" | cut -d" " -f1)
+        leave_e=$(echo $input | grep "leave" | cut -d" " -f1)
+        leave_d=$(echo $input | grep "verlässt" | cut -d" " -f1)
+        if [ "$joins_e" != "" -o "$joins_d" != "" ]
+        then
+                join="true"
+        elif [ "$leave_e" != "" -o  "$leave_d" != "" ]
+        then
+                leave="true"
+        fi
+}
 #########################################################################################
 
 #   Programm
